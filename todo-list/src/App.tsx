213943763683970ecc1a387615e84a3936b75194
DanilloRezende <danillo.rezende3@gmail.com>
2024-04-21 00:04:53 -0300

    Setup do projeto

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>OLA</h1>
+    </ThemeProvider>
+      
+  )
+}
+
+
---
+++ todo-list/src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle `
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    background-color: ${props => props.theme['gray-600']};
+    color: ${props => props.theme['gray-300']};
+    -webkit-font-smoothing: antialiased;
+}
+
+`
